5b53dc08dca1d5f440c261c91925a613ce6f4948
Atif Afzal <atif@atfzl.com>
2023-02-12 11:22:04 +0530

    Read annotation db file path

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
+import * as fs from "fs/promises";
+
 import { Notice, Plugin } from "obsidian";
+
+const ANNOTATION_DB_FOLDER_ABSOLUTE_PATH = `${process.env.HOME}/Library/Containers/com.apple.iBooksX/Data/Documents/AEAnnotation`;
 
 export default class AppleBooksPlugin extends Plugin {
 	async onload() {
+		const result = await fs.readdir(ANNOTATION_DB_FOLDER_ABSOLUTE_PATH);
+
+		const annotationDB = result
+			.filter((fileName) => fileName.endsWith(".sqlite"))
+			.first();
+
+		console.log(annotationDB);
+
 		// This creates an icon in the left ribbon.
 		this.addRibbonIcon("dice", "Sample Plugin", (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
