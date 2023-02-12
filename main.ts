aa492281d68d379b3fc7985e8cd41f93d0fc135e
Atif Afzal <atif@atfzl.com>
2023-02-12 11:35:40 +0530

    Read both db file names

--- main.ts
+++ main.ts
@@ -1,20 +1,36 @@
 import * as fs from "fs/promises";
+import * as path from "path";
 
 import { Notice, Plugin } from "obsidian";
 
-const ANNOTATION_DB_FOLDER_ABSOLUTE_PATH = `${process.env.HOME}/Library/Containers/com.apple.iBooksX/Data/Documents/AEAnnotation`;
+const APPLE_BOOKS_DATA_FOLDER_ABSOLUTE_PATH = `${process.env.HOME}/Library/Containers/com.apple.iBooksX/Data/Documents/`;
+
+const ANNOTATION_DB_FOLDER_ABSOLUTE_PATH = path.join(
+	APPLE_BOOKS_DATA_FOLDER_ABSOLUTE_PATH,
+	"AEAnnotation"
+);
+const BOOKS_DB_FOLDER_ABSOLUTE_PATH = path.join(
+	APPLE_BOOKS_DATA_FOLDER_ABSOLUTE_PATH,
+	"BKLibrary"
+);
 
 export default class AppleBooksPlugin extends Plugin {
 	async onload() {
 		const annotationDBFolderFiles = await fs.readdir(
 			ANNOTATION_DB_FOLDER_ABSOLUTE_PATH
 		);
-
 		const annotationDBFileName = annotationDBFolderFiles
 			.filter((fileName) => fileName.endsWith(".sqlite"))
 			.first();
+		console.log({ annotationDBFileName });
 
-		console.log(annotationDBFileName);
+		const booksDBFolderFiles = await fs.readdir(
+			BOOKS_DB_FOLDER_ABSOLUTE_PATH
+		);
+		const booksDBFileName = booksDBFolderFiles
+			.filter((fileName) => fileName.endsWith(".sqlite"))
+			.first();
+		console.log({ booksDBFileName });
 
 		// This creates an icon in the left ribbon.
 		this.addRibbonIcon("dice", "Sample Plugin", (evt: MouseEvent) => {
